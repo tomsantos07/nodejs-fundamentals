9b8346677a7d1fe5ee749e8a1ed1bf4fb5bdaf49
tomsantos07 <tomsantos07>
2022-04-05 14:26:33 -0300

    evoke the file 07 on app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const names = require('./04-names');
 const sayHi = require('./05-utils');
 const data = require('./06-alternative-flavor');
-console.log(data);
-sayHi('susan');
-sayHi(names.john);
-sayHi(names.peter);
+require('./07-mind-grenade');
+
+// sayHi('susan');
+// sayHi(names.john);
+// sayHi(names.peter);

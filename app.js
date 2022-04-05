03c939cf509d804670585d26a8bdbafdbc0ff4c4
tomsantos07 <tomsantos07>
2022-04-05 14:46:20 -0300

    evoke the built-in module and interacts with the OS

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-// CommonJS, every file is module (by default)
-// Modules - Encapsulated Code (only share minimum)
-const names = require('./04-names');
-const sayHi = require('./05-utils');
-const data = require('./06-alternative-flavor');
-require('./07-mind-grenade');
+const os = require('os');
 
-// sayHi('susan');
-// sayHi(names.john);
-// sayHi(names.peter);
+// info about current user
+const user = os.userInfo();
+
+// method returns the system uptime in seconds
+
+console.log(`The System Uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+
+console.log(currentOS);

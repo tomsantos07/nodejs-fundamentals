7a514fe819ae0c9eed25232b9d07107c2cb71256
tomsantos07 <tomsantos07>
2022-04-05 10:48:45 -0300

    requires e and log the datas from 06

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 // Modules - Encapsulated Code (only share minimum)
 const names = require('./04-names');
 const sayHi = require('./05-utils');
-
+const data = require('./06-alternative-flavor');
+console.log(data);
 sayHi('susan');
 sayHi(names.john);
 sayHi(names.peter);

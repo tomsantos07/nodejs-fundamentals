1618c2f9123f7c2488db7ec8cb5c334cbb60d0ab
tomsantos07 <tomsantos07>
2022-04-10 21:53:59 -0300

    put the file syn examples on the 10 file

---
+++ 10-fs-sync.js
@@ -0,0 +1,12 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+const first = readFileSync('./content/first.txt', 'utf-8');
+const second = readFileSync('./content/second.txt', 'utf-8');
+
+console.log(first, second);
+
+//  if there's not a file with the name, node will create one, if so, he'll overwrite
+writeFileSync('./content/result-sync.txt',
+  `Here is the result: ${first}, ${second}`,
+  { flag: 'a' }
+);

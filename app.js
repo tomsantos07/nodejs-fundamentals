001bc70b86e020a0fd6cd3ea55ab75f2bb960379
tomsantos07 <tomsantos07>
2022-04-16 09:08:24 -0300

    create a txt file by an async construction

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const { readFileSync, writeFileSync } = require('fs');
+const { readFile, writeFile } = require('fs');
 
-const first = readFileSync('./content/first.txt', 'utf-8');
-const second = readFileSync('./content/second.txt', 'utf-8');
-
-console.log(first, second);
-
-//  if there's not a file with the name, node will create one, if so, he'll overwrite
-writeFileSync('./content/result-sync.txt',
-  `Here is the result: ${first}, ${second}`,
-  { flag: 'a' }
-);
+readFile('./content/first.txt', 'utf-8', (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile('./content/second.txt', 'utf-8', (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile('./content/result-async.txt',
+      `Here is the result: ${first}, ${second}`
+    , (err, result)=> {
+      if(err) {
+        console.log(err);
+        return;
+      }
+      console.log(result);
+    })
+  })
+});

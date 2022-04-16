b51eeaf4548944e16237374a0fc4da93f03c5d39
tomsantos07 <tomsantos07>
2022-04-16 09:38:48 -0300

    added some logs in order to demonstrate the async execution

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,5 +1,6 @@
 const { readFile, writeFile } = require('fs');
 
+console.log('start');
 readFile('./content/first.txt', 'utf-8', (err, result) => {
   if (err) {
     console.log(err);
@@ -19,7 +20,9 @@ readFile('./content/first.txt', 'utf-8', (err, result) => {
         console.log(err);
         return;
       }
-      console.log(result);
+      console.log('done with this task');
     })
   })
 });
+
+console.log('starting the next task');
